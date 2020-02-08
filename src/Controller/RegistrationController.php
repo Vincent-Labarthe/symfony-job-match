796f54b086765a7b9ac0afdbe32cae37e141e5b6
Vincent Labarthe <v.labarthe@gmail.com>
2020-02-08 22:10:29 +0100

    esc upload on keyup

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\Type\RegistrationFormType;
+    use App\Form\Type\RegistrationFormType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -42,7 +42,7 @@ class RegistrationController extends AbstractController
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
-            $entityManager->flush();
+            $entityManager->flush();    
             $this->addFlash(
                 'success',
                 'Votre compte a bien été crée'
