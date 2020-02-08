eef3d08505fe1b171137e0f5c49f6f1b4e50a03e
Vincent Labarthe <v.labarthe@gmail.com>
2020-02-08 10:34:30 +0100

    logoutcontroller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,22 +3,36 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class UserController extends AbstractController{
+class UserController extends AbstractController
+{
 
 
     /**
      * @Route("/user/{id}", name="user_page")
      */
-    public function userPage (User $user){
+    public function userPage(User $user)
+    {
+        return $this->render(
+            'user/user_home_page.html.twig',
+            [
+                'user' => $user
+            ]
+        );
+    }
 
-        return $this->render('user/user_home_page.html.twig',
-    [
-        'user'=>$user
-    ]);
-        
+   /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function userLogOut()
+    {
+       
+       
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
 
+        return $this->redirectToRoute('home_page');
     }
 }
